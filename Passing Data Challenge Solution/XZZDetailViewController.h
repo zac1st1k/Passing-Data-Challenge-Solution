b58637345d88467769555992f958109a6a691299
zac1st1k <zacfirst@gmail.com>
2014-12-08 15:37:11 +1100

    + DetailViewController

---
+++ Passing Data Challenge Solution/XZZDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  XZZDetailViewController.h
+//  Passing Data Challenge Solution
+//
+//  Created by Zac on 8/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZDetailViewController : UIViewController
+
+@end
